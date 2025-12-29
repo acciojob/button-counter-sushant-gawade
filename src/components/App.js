5c135f14da3975b2a65d2aa65b05fa3aeddf5a08
Sushant Gawade <78841663+sushant-gawade@users.noreply.github.com>
2025-12-30 00:02:40 +0530

    Add ButtonCounter component to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 
 import React from "react";
 import './../styles/App.css';
+import ButtonCounter from "./ButtonCounter";
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+    <ButtonCounter />
     </div>
   )
 }
